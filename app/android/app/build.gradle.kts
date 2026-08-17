aebc8078eca403492278d1eb39bc5f3a71432d9a
Xavier Carol <xcarolr@gmail.com>
2026-08-17 17:45:34 +0200

    fix: min android api version to 26

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
 
     defaultConfig {
         applicationId = "com.xicra.bookscout"
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
